62b295b45119705a143d3a5516f153394cd751f3
MBattagl <mbattagl@methodsconsultants.com>
2019-07-01 13:01:54 -0400

    added weighting functionality to descriptives

--- R/descriptives.R
+++ R/descriptives.R
@@ -5,18 +5,39 @@
 #' @param df <`tbl_df`> Data frame with treatment and variables of interest as columns.
 #' @param variables <`character`> Character vector of column names. If left blank, will be inferred from data.
 #' @param treatment <`character(1)`> String with name of treatment column. If left blank, will produce univariate summary statistics.
+#' @param weights <`character(1)`> String with name of column with observation weights. If left blank, will not use observation weights.
 #'
 #' @return <`tbl_df`> Tibble with summary statistics split by treatment (optional).
 #'
 #' @import dplyr
 #' @import purrr
+#' @import assertthat
 #'
 #' @export
-descriptives <- function(df, treatment = NULL, variables = NULL) {
+descriptives <- function(df, treatment = NULL, variables = NULL, weights = NULL) {
+
+  assert_that(is.data.frame(df))
+
+  if (!is.null(treatment)) {
+    assert_that(is.string(treatment))
+    assert_that(is_categorical_variable(df, treatment))
+  }
+
+  if (!is.null(variables)) {
+    assert_that(is.character(variables))
+    assert_that(all(variables %in% colnames(df)))
+  }
+
+  if (!is.null(weights)) {
+    assert_that(is.string(weights))
+    assert_that(is.numeric(df[[weights]]))
+    assert_that(all(!is.na(df[[weights]])))
+    assert_that(all(df[[weights]] > 0))
+  }
 
   if (is.null(variables)) {
     variables <- colnames(df)
-    variables <- variables[!(variables %in% treatment)]
+    variables <- variables[!(variables %in% c(treatment, weights))]
   }
 
   cont_lgl <- variables %>%
@@ -40,17 +61,29 @@ descriptives <- function(df, treatment = NULL, variables = NULL) {
   }
 
   if (length(cat_vars) == 0) {
-    cont_tbl <- mean_sd_func(df, cont_vars, treatment)
+    cont_tbl <- mean_sd_func(df, cont_vars, treatment, weights)
     return(cont_tbl)
   }
 
   if (length(cont_vars) == 0) {
-    cat_tbl <- cat_func(df, cat_vars, treatment)
+    cat_tbl <- cat_func(df, cat_vars, treatment, weights)
     return(cat_tbl)
   }
 
-  cont_tbl <- mean_sd_func(df, cont_vars, treatment)
-  cat_tbl <- cat_func(df, cat_vars, treatment)
+  if (!is.null(weights) & !is.null(treatment)) {
+
+    weight_mean <- df %>%
+      drop_na(!!treatment) %>%
+      pull(!!weights) %>%
+      mean()
+
+    if (weight_mean != 1) {
+      message("Weights were normalized to a mean of 1 to preserve sample size in significance tests")
+    }
+  }
+
+  cont_tbl <- mean_sd_func(df, cont_vars, treatment, weights)
+  cat_tbl <- cat_func(df, cat_vars, treatment, weights)
 
   bind_rows(cat_tbl, cont_tbl) %>%
     tidyr::replace_na(list(Label = ""))
@@ -71,30 +104,47 @@ descriptives <- function(df, treatment = NULL, variables = NULL) {
 #' @import tidyr
 #'
 #' @noRd
-cat_func <- function(df, cat_vars, treatment) {
+cat_func <- function(df, cat_vars, treatment, weights) {
 
   cat_vars <- syms(cat_vars)
 
   df <- df %>%
     mutate_if(is.factor, as.character)
 
+  if (is.null(weights)) {
+
+    df <- df %>%
+      mutate(obs_weights = 1)
+
+    weighted <- FALSE
+
+  } else {
+
+    df <- df %>%
+      rename(obs_weights = weights)
+
+    weighted <- TRUE
+
+  }
+
+
   if (is.null(treatment)) {
 
     df %>%
-      count(!!!cat_vars) %>%
+      select(!!!cat_vars, obs_weights) %>%
       gather(
         key = Variable,
         value = Label,
         !!!cat_vars
       ) %>%
       group_by(Variable, Label) %>%
-      summarise_all(sum) %>%
+      summarise(weighted_count = sum(obs_weights)) %>%
       ungroup() %>%
       drop_na() %>%
       group_by(Variable) %>%
-      mutate(Statistics = proportions(n)) %>%
+      mutate(Statistics = proportions(weighted_count, weighted = weighted)) %>%
       ungroup() %>%
-      select(-n)
+      select(-weighted_count)
 
   } else {
 
@@ -106,25 +156,25 @@ cat_func <- function(df, cat_vars, treatment) {
     p_chi_fisher <- possibly(p_chi_fisher, NA_real_)
 
     df %>%
-      count(!!treatment, !!!cat_vars) %>%
+      select(!!treatment, !!!cat_vars, obs_weights) %>%
       gather(
         key = Variable,
         value = Label,
         !!!cat_vars
       ) %>%
       group_by(!!treatment, Variable, Label) %>%
-      summarise_all(sum) %>%
+      summarise(weighted_count = sum(obs_weights)) %>%
       ungroup() %>%
       drop_na() %>%
       group_by(!!treatment, Variable) %>%
-      mutate(Statistics = proportions(n)) %>%
+      mutate(Statistics = proportions(weighted_count, weighted = weighted)) %>%
       ungroup() %>%
-      select(-n) %>%
+      select(-weighted_count) %>%
       spread(
         key = !!treatment,
         value = Statistics
       ) %>%
-      mutate(`P Value` = map_dbl(Variable, p_chi_fisher, df = df, treatment = quo_name(treatment)))
+      mutate(`P Value` = map_dbl(Variable, p_chi_fisher, df = df, treatment = quo_name(treatment), weight_var = "obs_weights"))
 
   }
 }
@@ -143,10 +193,28 @@ cat_func <- function(df, cat_vars, treatment) {
 #' @import tidyr
 #'
 #' @noRd
-mean_sd_func <- function(df, cont_vars, treatment) {
+mean_sd_func <- function(df, cont_vars, treatment, weights) {
 
-  Mean <- partial(mean, na.rm = TRUE)
-  SD <- partial(sd, na.rm = TRUE)
+  if (!is.null(treatment)) {
+
+    treatment <- sym(treatment)
+
+    df <- df %>%
+      drop_na(!!treatment)
+
+  }
+
+  if (is.null(weights)) {
+
+    df <- df %>%
+      mutate(obs_weights = 1)
+
+  } else {
+
+    df <- df %>%
+      rename(obs_weights = weights)
+
+  }
 
   ## Set names to force consistent syntax in summarise_at
   var_named <- cont_vars %>%
@@ -155,7 +223,13 @@ mean_sd_func <- function(df, cont_vars, treatment) {
   if (is.null(treatment)) {
 
     df %>%
-      summarise_at(var_named, list("Mean" = Mean, "SD" = SD)) %>%
+      summarise_at(
+        .vars = var_named,
+        .funs = list(
+          "Mean" = ~weighted_mean(.x, obs_weights),
+          "SD" = ~weighted_sd(.x, obs_weights)
+        )
+      ) %>%
       gather(
         key = "Variable",
         value = "val"
@@ -175,23 +249,24 @@ mean_sd_func <- function(df, cont_vars, treatment) {
 
   } else {
 
-    treatment <- sym(treatment)
-
-    df <- df %>%
-      drop_na(!!treatment)
-
     p_anova <- possibly(p_anova, NA_real_)
 
     df %>%
       group_by(!!treatment) %>%
-      summarise_at(var_named, list("Mean" = Mean, "SD" = SD)) %>%
+      summarise_at(
+        .vars = var_named,
+        .funs = list(
+          "Mean" = ~weighted_mean(.x, obs_weights),
+          "SD" = ~weighted_sd(.x, obs_weights)
+        )
+      ) %>%
       gather("Variable", "val", -!!treatment) %>%
       separate(Variable, c("Variable", "mean_sd"), "_(?!.*_)") %>%
       spread(key = mean_sd, value = val) %>%
       mutate(mean_sd = paste0(round(Mean, 2), " (", round(SD, 2), ")")) %>%
       select(-Mean, -SD) %>%
       spread(!!treatment, mean_sd) %>%
-      mutate(`P Value` = map_dbl(Variable, p_anova, df = df, treatment = quo_name(treatment)))
+      mutate(`P Value` = map_dbl(Variable, p_anova, df = df, treatment = quo_name(treatment), weight_var = "obs_weights"))
 
   }
 }
